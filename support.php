5de857518a8586c8a14f4f9692ee59b2b97dbd33
eric <ecomstock@gmail.com>
2019-08-02 16:48:57 -0700

    update: add copy to "support" page

--- support.php
+++ support.php
@@ -3,7 +3,80 @@
 	<main role="main">
 
         <div class="container">
-            
+            <p>The West Linn Community Chorus is a 501 (c)(3) non-profit organization dependent on the generosity of our community. We are funded through grants, ticket sales, fundraisers, membership fees and private donations. Support us through our Friends of the Chorus program or by shopping through our Amazon Smile or Sheet Music Plus links.</p>
+            <h3>Friends of the Chorus</h3>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div>
+                        <p>Donations to our Friends of the Chorus program enable us to pay our professional musicians, rent rehearsal and performance spaces, promote our events, and provide music to all of our members.</p>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+            <h3>Donation Levels </h3>
+            <table class="table">
+                <tbody>
+                    <tr>
+                        <td scope="row">Maestro</td>
+                        <td>$1,000 and up</td>
+                        <td>8 complimentary tickets and program acknowledgement.</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Virtuoso</td>
+                        <td>$500 – $999</td>
+                        <td>6 complimentary tickets and program acknowledgement.</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Prodigy</td>
+                        <td>$200 – $499</td>
+                        <td>5 complimentary tickets and program acknowledgement.</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Whole Note</td>
+                        <td>$100-$199</td>
+                        <td>4 complimentary tickets and program acknowledgement.</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Half Note</td>
+                        <td>$50-$99</td>
+                        <td>2 complimentary tickets and program acknowledgement.</td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Quarter Note</td>
+                        <td>$25-$49</td>
+                        <td>Program acknowledgement.</td>
+                    </tr>
+                </tbody>
+            </table>
+            <p>Kindly mail donations to: West Linn Community Chorus, PO Box 187, West Linn, OR 97068</p>
+            <p>Please write “Friends of the Chorus” in the memo line.</p>
+            <h3>Amazon Smile</h3>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div><p>Support the chorus while you shop. Simply login through the AmazonSmile portal and designate the West Linn Community Chorus as your charity and Amazon will donate 0.5% of the price of your eligible purchases to West Linn Community Chorus.</p></div>
+                </div>
+                <div class="col-sm-6">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+            <p>Simply follow this link to AmazonSmile and login to your Amazon account as normal.</p>
+            <h3>Sheet Music Plus</h3>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div><p>Help support the Chorus through your music purchases!  Any music purchased through the  link at right earns the Chorus an 8% rebate which in turn helps grow our library with fresh new music and arrangements.</p></div>
+                </div>
+                <div class="col-sm-6">
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
         </div>
 
     </main>
