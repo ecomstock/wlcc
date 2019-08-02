fbc501b0b47f79eefb2a037cd40e56af449730bb
eric <ecomstock@gmail.com>
2019-08-02 16:49:52 -0700

    update: add copy to "events" page

--- events.php
+++ events.php
@@ -3,7 +3,12 @@
 	<main role="main">
 
         <div class="container">
-            
+            <h2>EVENTS</h2>
+            <h3>Winter 2019 Concert: A Celtic Holiday</h3>
+            <p>December – TBA</p>
+            <h3>Spring 2020 Concert: Blame it on the Silver Screen</h3>
+            <p>May – TBA</p>
+            <h3>Past Concerts:</h3>
         </div>
 
     </main>
