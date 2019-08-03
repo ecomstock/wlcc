782b114e43b465e7c59fa82a6e184eb1c30ecba0
eric <ecomstock@gmail.com>
2019-08-02 18:58:39 -0700

    update: style "join" page

--- join.php
+++ join.php
@@ -2,48 +2,45 @@
 
 	<main role="main">
 
-        <div class="container">
-            <p>When can I join?</p>
+        <div class="container narrow-container">
+            <h2 class="mb-5">Join Us!</h2>
             <div class="row">
-                <div class="col-sm-6">
-                    <div>
-                        <p>New singers can join the chorus at the beginning of both the fall and spring concert seasons (September and January). Singers can come to any or all of the first three rehearsals before committing.  If you are interested, please email us at westlinnchorus@gmail.com.  We will notify you of dates, times, fees and other details.</p>
-                    </div>
+                <div class="col-md-6 mb-5">
+                    <p><strong>When can I join?</strong></p>
+                    <p>New singers can join the chorus at the beginning of both the fall and spring concert seasons (September and January). Singers can come to any or all of the first three rehearsals before committing.  If you are interested, please email us at westlinnchorus@gmail.com.  We will notify you of dates, times, fees and other details.</p>
+                    <p class="mt-5"><strong>When do we rehearse?</strong></p>
+                    <p>We meet each Thursday at 7:00 pm at Emmanuel Presbyterian Church in West Linn. There are a handful of additional evening or weekend rehearsals each season. We ask that members plan to attend all rehearsals and notify their section leader if they need to be absent.</p>
                 </div>
-                <div class="col-sm-6">
-                    <div>
-                        <img src="" alt="">
-                    </div>
-                </div>
-            </div>
-            <p>When do we rehearse?</p>
-            <p>We meet each Thursday at 7:00 pm at Emmanuel Presbyterian Church in West Linn. There are a handful of additional evening or weekend rehearsals each season. We ask that members plan to attend all rehearsals and notify their section leader if they need to be absent.</p>
-            <p>Who can join?</p>
-            <div class="row">
-                <div class="col-sm-6">
-                    <div>
-                        <p>As a true community chorus, we welcome anyone who can carry a tune. No auditions are required. While most members have prior choral singing experience and many read music, others pick up their parts with the help of rehearsals, sectional rehearsals, and practice CDs. Although we meet in West Linn, our members gather from all over the Portland metro area.</p>
-                    </div>
-                </div>
-                <div class="col-sm-6">
-                    <div>
-                        <img src="" alt="">
-                    </div>
+                <div class="col-md-6 mb-5">
+                    <figure class="figure">
+                        <img src="./img/rehearsal-1.jpg" class="figure-img img-fluid rounded" alt="">
+                    </figure>
                 </div>
             </div>
             <div class="row">
-                <div class="col-sm-6">
+                <div class="col-md-6">
+                    <p><strong>Who can join?</strong></p>
+                    <p class="mb-5">As a true community chorus, we welcome anyone who can carry a tune. No auditions are required. While most members have prior choral singing experience and many read music, others pick up their parts with the help of rehearsals, sectional rehearsals, and practice CDs. Although we meet in West Linn, our members gather from all over the Portland metro area.</p>
+                </div>
+                <div class="col-md-6 mb-5">
+                    <figure class="figure">
+                        <img src="./img/chorus-women-3.jpg" class="figure-img img-fluid rounded" alt="">
+                    </figure>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
                     <div>
-                        <p>What about performances?</p>
+                        <p><strong>What about performances?</strong></p>
                         <p>In addition to our annual fall and spring concerts, we send small groups out into the community to perform at senior centers and local events.</p>
-                        <p>What kind of music do we sing?</p>
+                        <p class="mt-5"><strong>What kind of music do we sing?</strong></p>
                         <p>We sing all kinds of music: classical, popular, traditional, songs from Broadway and more.</p>
                     </div>
                 </div>
-                <div class="col-sm-6">
-                    <div>
-                        <img src="" alt="">
-                    </div>
+                <div class="col-md-6">
+                    <figure class="figure">
+                        <img src="./img/rehearsal-2.jpg" class="figure-img img-fluid rounded" alt="">
+                    </figure>
                 </div>
             </div>
         </div>
