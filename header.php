731a6610073f1490cd47a5b63b7a7ddd2b8b7107
eric <ecomstock@gmail.com>
2019-09-03 10:01:22 -0700

    rm: delete extraneous html & php

--- header.php
+++ header.php
@@ -17,45 +17,10 @@
 </head>
 
 <body>
-	<!-- <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-		<a class="navbar-brand" href="./index.php">
-			<img src="./img/logo.jpg" class="figure-img img-fluid mb-0" alt="" style="max-width: 60px;">
-		</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-			aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-
-		<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-			<ul class="navbar-nav mr-auto">
-				<?php	
-					// $urls = array(
-					// 	"Home"    => "./index.php",
-					// 	"About"   => "./about.php",
-					// 	"Support" => "./support.php",
-					// 	"Join"    => "./join.php",
-					// 	"Events"  => "./events.php",
-					// 	"Contact" => "./contact.php"
-					// );
-					// foreach ($urls as $name => $url) {
-					// 	$listClass = (($currentPage === $name) ? "class='nav-item active'" : "class='nav-item'");
-					// 	$SRSpan = (($currentPage === $name) ? "<span class='sr-only'>(current)</span>" : "");
-
-					// 	print 
-					// 		"<li $listClass>
-					// 			<a class='nav-link' href='$url'>$name $SRSpan</a>
-					// 		</li>";
-					// }
-				?>
-			</ul>
-		</div>
-	</nav> -->
-
 	<div class="flex-container">
 		<header class="masthead pt-3 pl-3">
 			<div class="inner">
 				<nav class="nav nav-masthead justify-content-center">
-					<!-- <a class="nav-link active" href="#">Home</a> -->
 					<?php	
 						$urls = array(
 							"Home"    => "./index.php",
@@ -67,7 +32,6 @@
 						);
 						foreach ($urls as $name => $url) {
 							$aClass = (($currentPage === $name) ? "class='nav-link active'" : "class='nav-link'");
-							// $SRSpan = (($currentPage === $name) ? "<span class='sr-only'>(current)</span>" : "");
 	
 							print 
 								"<a $aClass href='$url'>$name</a>";
