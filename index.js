6b6edc5a419439ef69a5936c796543ca18ed3ee8
eric <ecomstock@gmail.com>
2019-09-25 13:49:56 -0700

    update: add footer loader

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 $(document).ready(function(){
 
-        
     loadHeader();
+    loadFooter();
+    loadDate();
 
     function loadHeader (callback) {
         const $header = $("#header");
-        $header.load("./header.html", navActivator)
+        $header.load("header.html", navActivator);
     }
+
+    function loadFooter () {
+        const $footer = $("#footer");
+        $footer.load("footer.html");
+    }
+
+    
 
     function navActivator () {
         //$("#home").addClass("active");
