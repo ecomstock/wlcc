e7aba9b9f9ab6155e12602137aac404a028a672e
eric <ecomstock@gmail.com>
2019-09-25 18:15:15 -0700

    update: fix js date functions

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ $(document).ready(function(){
 
     function loadFooter () {
         const $footer = $("#footer");
-        $footer.load("footer.html");
+        $footer.load("footer.html", loadDate);
     }
 
-    
+    function loadDate () {
+        const year = getYear().toString();
+        const $date = $("#date");
+        $date.html(`&copy; WLCC ${year}`);
+    }
+
+    function getYear () {
+        const year = new Date().getFullYear();
+        return year
+    }
 
     function navActivator () {
         //$("#home").addClass("active");
