0724e091a04c347c28e4e8e9336b03a9261982a2
eric <ecomstock@gmail.com>
2019-10-15 18:49:39 -0700

    update: rm date function (add to utilities.js)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import utilities from "./utilities.js";
+
 $(document).ready(function(){
 
     loadHeader();
@@ -15,20 +17,14 @@ $(document).ready(function(){
     }
 
     function loadDate () {
-        const year = getYear().toString();
+        const year = utilities.getYear().toString();
         const $date = $("#date");
         $date.html(`&copy; WLCC ${year}`);
-    }
-
-    function getYear () {
-        const year = new Date().getFullYear();
-        return year
     }
 
     function activateNav () {
         const clippedPath = location.pathname.split("wlcc/")[1];
         const $activeElement = $(`a[href='${clippedPath}']`);
         $activeElement.addClass("active");
-        //$('a[href="./' + location.pathname.split("wlcc/")[1] + '"]').addClass("active");
     }
 });
